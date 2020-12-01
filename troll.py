5dcb1d02b3ef69d245bf2209844b348343707bf1
Huy-DaraPichchan <huy.darapichchan19@kit.edu.kh>
2020-12-01 13:22:53 +0700

    third push by chan

--- troll.py
+++ troll.py
@@ -1,8 +1,12 @@
 user_input = input("Wanna play a game? y/n")
+isEnabling = False
 if user_input == 'Y' or user_input == 'y':
     print("This is infinite loop haha goodluck")
     isEnabling = True
     while isEnabling:
         print("You are being trolled")
 elif user_input == 'N' or user_input == 'n':
-    print("You are lucky")
+    print("You are lucky but still")
+    isEnabling = True
+    while isEnabling:
+        print("You are being trolled still")
