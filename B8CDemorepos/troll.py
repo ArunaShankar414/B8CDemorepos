5534a71688521de85c2a45fb544594d38ae1a087
YinChantha <yin.chantha19@kit.edu.kh>
2020-12-03 19:55:24 +0700

    This my second pracetices

---
+++ B8CDemorepos/troll.py
@@ -0,0 +1,8 @@
+user_input = input("Wanna play a game? y/n")
+if user_input == 'Y' or user_input == 'y':
+    print("This is infinite loop haha goodluck")
+    isEnabling = True
+    while isEnabling:
+        print("You are being trolled")
+elif user_input == 'N' or user_input == 'n':
+    print("You are lucky")
